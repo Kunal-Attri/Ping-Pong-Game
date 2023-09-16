1ca3dd91f49d8e4e69b6487848c29a690075c652
Kunal Attri <attrikunal16@gmail.com>
2023-09-16 22:48:56 +0530

    refractored non-final variables to lower case

--- Pong_But_Better.py
+++ Pong_But_Better.py
@@ -18,11 +18,11 @@ RED = (255, 0, 0)
 
 # BALL INIT
 RADIUS = 15
-BALL_X = WIDTH / 2 - RADIUS
-BALL_Y = HEIGHT / 2 - RADIUS
+ball_x = WIDTH / 2 - RADIUS
+ball_y = HEIGHT / 2 - RADIUS
 DEFAULT_BALL_VELOCITY = 0.08
-BALL_VELOCITY_X = DEFAULT_BALL_VELOCITY
-BALL_VELOCITY_Y = DEFAULT_BALL_VELOCITY
+ball_velocity_x = DEFAULT_BALL_VELOCITY
+ball_velocity_y = DEFAULT_BALL_VELOCITY
 
 # PADDLES INIT
 PADDLE_WIDTH = 20
@@ -30,11 +30,11 @@ PADDLE_HEIGHT = 120
 PADDLE_VELOCITY_UP = -0.15
 PADDLE_VELOCITY_DOWN = 0.15
 LEFT_PADDLE_X = 100 - PADDLE_WIDTH / 2
-LEFT_PADDLE_Y = HEIGHT / 2 - PADDLE_HEIGHT / 2
+left_paddle_y = HEIGHT / 2 - PADDLE_HEIGHT / 2
 RIGHT_PADDLE_X = WIDTH - LEFT_PADDLE_X
-RIGHT_PADDLE_Y = LEFT_PADDLE_Y
-LEFT_PADDLE_VELOCITY = 0
-RIGHT_PADDLE_VELOCITY = LEFT_PADDLE_VELOCITY
+right_paddle_y = left_paddle_y
+left_paddle_velocity = 0
+right_paddle_velocity = 0
 
 # MAIN LOOP
 run = True
@@ -47,64 +47,64 @@ while run:
         elif event.type == pygame.KEYDOWN:
             # Right Player - Up and Down keys
             if event.key == pygame.K_UP:
-                RIGHT_PADDLE_VELOCITY = PADDLE_VELOCITY_UP
+                right_paddle_velocity = PADDLE_VELOCITY_UP
             if event.key == pygame.K_DOWN:
-                RIGHT_PADDLE_VELOCITY = PADDLE_VELOCITY_DOWN
+                right_paddle_velocity = PADDLE_VELOCITY_DOWN
 
             # Left Player - W and S keys
             if event.key == pygame.K_w:
-                LEFT_PADDLE_VELOCITY = PADDLE_VELOCITY_UP
+                left_paddle_velocity = PADDLE_VELOCITY_UP
             if event.key == pygame.K_s:
-                LEFT_PADDLE_VELOCITY = PADDLE_VELOCITY_DOWN
+                left_paddle_velocity = PADDLE_VELOCITY_DOWN
 
         if event.type == pygame.KEYUP:
-            LEFT_PADDLE_VELOCITY = 0
-            RIGHT_PADDLE_VELOCITY = 0
+            left_paddle_velocity = 0
+            right_paddle_velocity = 0
 
     # Ball's Movement Controls
     # When ball touches the top or bottom of screen
-    if BALL_Y <= 0 + RADIUS or BALL_Y >= HEIGHT - RADIUS:
-        BALL_VELOCITY_Y *= -1
+    if ball_y <= 0 + RADIUS or ball_y >= HEIGHT - RADIUS:
+        ball_velocity_y *= -1
 
     # When ball goes out of left or right window -> Ball must reset
-    if BALL_X <= 0 + RADIUS or BALL_X >= WIDTH - RADIUS:
-        BALL_X = WIDTH / 2 - RADIUS
-        BALL_Y = HEIGHT / 2 - RADIUS
+    if ball_x <= 0 + RADIUS or ball_x >= WIDTH - RADIUS:
+        ball_x = WIDTH / 2 - RADIUS
+        ball_y = HEIGHT / 2 - RADIUS
 
         ball_direction = random.choice(DIRECTION)
         ball_angle = random.choice(ANGLE)
-        BALL_VELOCITY_X = DEFAULT_BALL_VELOCITY if BALL_VELOCITY_X < 0 else -DEFAULT_BALL_VELOCITY
-        BALL_VELOCITY_X = ball_angle[0] * BALL_VELOCITY_X
-        BALL_VELOCITY_Y = ball_direction * ball_angle[1] * DEFAULT_BALL_VELOCITY
+        ball_velocity_x = DEFAULT_BALL_VELOCITY if ball_velocity_x < 0 else -DEFAULT_BALL_VELOCITY
+        ball_velocity_x = ball_angle[0] * ball_velocity_x
+        ball_velocity_y = ball_direction * ball_angle[1] * DEFAULT_BALL_VELOCITY
 
     # Paddle's Movement Controls
-    if LEFT_PADDLE_Y <= 0:
-        LEFT_PADDLE_Y = 0
-    elif LEFT_PADDLE_Y >= HEIGHT - PADDLE_HEIGHT:
-        LEFT_PADDLE_Y = HEIGHT - PADDLE_HEIGHT
-    if RIGHT_PADDLE_Y <= 0:
-        RIGHT_PADDLE_Y = 0
-    elif RIGHT_PADDLE_Y >= HEIGHT - PADDLE_HEIGHT:
-        RIGHT_PADDLE_Y = HEIGHT - PADDLE_HEIGHT
+    if left_paddle_y <= 0:
+        left_paddle_y = 0
+    elif left_paddle_y >= HEIGHT - PADDLE_HEIGHT:
+        left_paddle_y = HEIGHT - PADDLE_HEIGHT
+    if right_paddle_y <= 0:
+        right_paddle_y = 0
+    elif right_paddle_y >= HEIGHT - PADDLE_HEIGHT:
+        right_paddle_y = HEIGHT - PADDLE_HEIGHT
 
     # Collisions
     # Left Paddle
-    if LEFT_PADDLE_X <= BALL_X <= LEFT_PADDLE_X + PADDLE_WIDTH and LEFT_PADDLE_Y <= BALL_Y <= LEFT_PADDLE_Y + PADDLE_HEIGHT:
-        BALL_X = LEFT_PADDLE_X + PADDLE_WIDTH
-        BALL_VELOCITY_X *= -1
+    if LEFT_PADDLE_X <= ball_x <= LEFT_PADDLE_X + PADDLE_WIDTH and left_paddle_y <= ball_y <= left_paddle_y + PADDLE_HEIGHT:
+        ball_x = LEFT_PADDLE_X + PADDLE_WIDTH
+        ball_velocity_x *= -1
     # Right Paddle
-    if RIGHT_PADDLE_X <= BALL_X <= RIGHT_PADDLE_X + PADDLE_WIDTH and RIGHT_PADDLE_Y <= BALL_Y <= RIGHT_PADDLE_Y + PADDLE_HEIGHT:
-        BALL_X = RIGHT_PADDLE_X
-        BALL_VELOCITY_X *= -1
+    if RIGHT_PADDLE_X <= ball_x <= RIGHT_PADDLE_X + PADDLE_WIDTH and right_paddle_y <= ball_y <= right_paddle_y + PADDLE_HEIGHT:
+        ball_x = RIGHT_PADDLE_X
+        ball_velocity_x *= -1
 
     # Movements
-    BALL_X += BALL_VELOCITY_X
-    BALL_Y += BALL_VELOCITY_Y
-    LEFT_PADDLE_Y += LEFT_PADDLE_VELOCITY
-    RIGHT_PADDLE_Y += RIGHT_PADDLE_VELOCITY
+    ball_x += ball_velocity_x
+    ball_y += ball_velocity_y
+    left_paddle_y += left_paddle_velocity
+    right_paddle_y += right_paddle_velocity
 
     # Objects - Balls and Paddles
-    pygame.draw.circle(window, BLUE, (BALL_X, BALL_Y), RADIUS)
-    pygame.draw.rect(window, RED, pygame.Rect(LEFT_PADDLE_X, LEFT_PADDLE_Y, PADDLE_WIDTH, PADDLE_HEIGHT))
-    pygame.draw.rect(window, RED, pygame.Rect(RIGHT_PADDLE_X, RIGHT_PADDLE_Y, PADDLE_WIDTH, PADDLE_HEIGHT))
+    pygame.draw.circle(window, BLUE, (ball_x, ball_y), RADIUS)
+    pygame.draw.rect(window, RED, pygame.Rect(LEFT_PADDLE_X, left_paddle_y, PADDLE_WIDTH, PADDLE_HEIGHT))
+    pygame.draw.rect(window, RED, pygame.Rect(RIGHT_PADDLE_X, right_paddle_y, PADDLE_WIDTH, PADDLE_HEIGHT))
     pygame.display.update()
